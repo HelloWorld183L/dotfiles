e50cbff5788276d1d3d39c7cf1f92590d9a35c1b
Leon Harper <60989@INTERNET.CTPNW.UK>
2025-12-05 13:02:10 +0000

    init

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y neovim
+curl https://install.duckdb.org | sh
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+
+export PATH='/home/coder/.duckdb/cli/latest':$PATH
